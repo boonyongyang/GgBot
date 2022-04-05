f1150aada651ec0e52239f98ea94f6df200700ad
Annebel <annebelchoong@gmail.com>
2022-04-06 00:15:32 +0800

    fourth bone and joint added

--- Main.cpp
+++ Main.cpp
@@ -393,6 +393,21 @@ void drawSpine2() {
 			drawSpineJoint();
 		}
 		glPopMatrix();
+
+		glPushMatrix();
+		{
+			glColor3f(1.0, 0.0, 1.0);
+			glTranslatef(0.0, 2.4, 0.0);
+			renderCube(0.1, 0.4, 0.05);
+		}
+		glPopMatrix();
+
+		glPushMatrix();
+		{
+			glTranslatef(0.0, 2.8, 0.0);
+			drawSpineJoint();
+		}
+		glPopMatrix();
 	}
 	glPopMatrix();
 }
