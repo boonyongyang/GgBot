0010360b9ed79134608dee2cbd525759c901e2f9
Annebel <annebelchoong@gmail.com>
2022-04-09 16:45:04 +0800

    third core bottom left done

--- Main.cpp
+++ Main.cpp
@@ -764,6 +764,20 @@ void drawCore6Packs() {
 			glPopMatrix();
 		}
 		glPopMatrix();
+
+		glPushMatrix();
+		{
+			glTranslatef(-0.5, 0.0, -1.1);
+			glPushMatrix();
+			{
+				glColor3f(0.0, 1.0, 1.0);
+				glTranslatef(-1, -1, 0.0);
+				glRotatef(-16, 0.0, 0.0, 1.0);
+				renderTrapezoidWithoutGLU(1.3, 0.4, 1.5, 0.7, 0.4);
+			}
+			glPopMatrix();
+		}
+		glPopMatrix();
 		
 	}
 	glPopMatrix();
